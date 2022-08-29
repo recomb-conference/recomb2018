47eb866360ad16be18b1a22423019124ca0cf7f7
calkan <calkan@gmail.com>
2022-08-29 18:44:35 +0300

    init

---
+++ xmlrpc.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>http://recomb2018.fr</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="http://recomb2018.fr/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://recomb2018.fr/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://recomb2018.fr/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="http://recomb2018.fr/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="http://recomb2018.fr/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
